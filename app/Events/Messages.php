86b64cd820750a77bb98a23b0206767f5d92f6e5
Tarik Ouhamou <tarik.ouhamou@neoxia.com>
2021-08-19 23:24:39 +0100

    first

---
+++ app/Events/Messages.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class Messages
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $body;
+    public $receiver;
+    public $sender;
+    public $data;
+
+    public function __construct($body,$receiver,$sender,$data)
+    {
+        $this->body=$body;
+        $this->receiver=$receiver;
+        $this->sender=$sender;
+        $this->data=$data;
+
+    }
+
+    public function broadcastOn()
+    {
+        return new Channel('my-channel-'.$this->receiver);
+    }
+
+    public function broadcastAs()
+    {
+        return 'my-event';
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    public function register(Request $request){
+        $user = User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>$request->password,
+        ]);
+
+        return response()->json(['user'=>$user,'message'=>'Succefully registered'],200);
+    }
+}
