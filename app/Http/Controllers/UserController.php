e7b6da2b8eb826774f58c57bd2c947c5f113356f
Tarik Ouhamou <tarik.ouhamou@neoxia.com>
2021-09-15 13:20:33 +0100

    testing

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\User;
+use Illuminate\Support\Facades\Http;
 
 use Illuminate\Http\Request;
 
@@ -9,17 +10,26 @@ class UserController extends Controller
 {
 
     public function index(){
-        $user = User::all();
-        return response()->json($user);
+        /*$user = User::all();
+        return response()->json($user);*/
+        $response = Http::get('https://odhiya.com/get.php');
+        return $response->body();
     }
 
     public function register(Request $request){
-        $user = User::create([
+        /*$user = User::create([
             'name'=>$request->name,
             'email'=>$request->email,
             'password'=>$request->password,
         ]);
 
-        return response()->json(['user'=>$user,'message'=>'Succefully registered'],200);
+        return response()->json(['user'=>$user,'message'=>'Succefully registered'],200);*/
+        $response = Http::post('https://odhiya.com/post_data.php', [
+            'sender_id' => 1,
+            'receiver_id' => 2,
+            'body' => "7izb lgar3a"
+        ]);
+        
+        return $response;
     }
 }
